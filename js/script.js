f5ebddd8641b377ac5b1b6fbb1737e21acd808f0
Marc Sanchez <marcsanchez@me.com>
2020-08-18 12:14:36 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Sticky navigation
+// $(".main-nav").sticky({
+//   topSpacing: 0
+// });
+
+// const readAbout = document.querySelector('.read-about');
+// const readMoreBtn = document.querySelector('.read-more-btn');
+
+// readAbout.style.display = "none";
+
+// readMoreBtn.addEventListener('click', () => {
+//   if (readAbout.style.display == "none") {
+//     readAbout.style.display = "block";
+//     readMoreBtn.textContent = "Read Less About Us";
+//     readMoreBtn.style.backgroundColor = "#ea2127";
+//     readMoreBtn.style.color = "#FFF";
+//   } else {
+//     readAbout.style.display = "None";
+//     readMoreBtn.textContent = "Read More About Us";
+//     readMoreBtn.style.backgroundColor = "#FFF";
+//     readMoreBtn.style.color = "#ea2127";
+//   }
+// });
+
